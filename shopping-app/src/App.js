d9a9ed08b5459e2d40e0c9ba1d03f5d6328759b3
Kunal Rajesh Salunkhe <salunkhekunal594@gmail.com>
2023-02-13 23:14:03 +0530

    Added Checkout Feature

--- shopping-app/src/App.js
+++ shopping-app/src/App.js
@@ -4,6 +4,8 @@ import Header from './components/Header/Header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
+import Summary from './pages/Summary';
+import Checkout from './pages/Checkout'
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/product/cart' element={<Cart/>}/>
           <Route path='/product/:id' element={<ProductDetails/>}/>
+          <Route path='/summary' element={<Summary/>}/>
+          <Route path='/checkout' element={<Checkout/>}/>
         </Routes>
       </Router>
     </div>
--- shopping-app/src/pages/Cart.jsx
+++ shopping-app/src/pages/Cart.jsx
@@ -1,9 +1,20 @@
 import ProductList from "../components/Product/ProductList";
+import { Link } from "react-router-dom";
+import { RiBillFill } from "react-icons/ri";
+import "./Pages.css";
 
 function Cart() {
   const cartProducts = JSON.parse(localStorage.getItem("Cart"));
   return cartProducts ? (
-    <ProductList products={cartProducts} />
+    <>
+      <h1 className="heading">Cart</h1>
+      <ProductList products={cartProducts} />
+      <Link to="/summary">
+        <button className="proceed-btn">
+          Proceed <RiBillFill />
+        </button>
+      </Link>
+    </>
   ) : (
     <h2 style={{ textAlign: "center", marginTop: "10rem" }}>
       Add Products to Cart
